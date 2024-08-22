83dc9a441e6b049b92f05e7e065e32aea0336960
Jakub <jakub@schwenkbeck.de>
2024-08-22 22:56:33 +0200

    Enabled Usernames for devise and db

---
+++ app/controllers/users/registrations_controller.rb
@@ -0,0 +1,14 @@
+class Users::RegistrationsController < Devise::RegistrationsController
+  before_action :configure_sign_up_params, only: [:create]
+  before_action :configure_account_update_params, only: [:update]
+
+  protected
+
+  def configure_sign_up_params
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
+  end
+
+  def configure_account_update_params
+    devise_parameter_sanitizer.permit(:account_update, keys: [:username])
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_08_22_202734) do
+ActiveRecord::Schema[7.2].define(version: 2024_08_22_204209) do
   create_table "posts", force: :cascade do |t|
     t.integer "user_id", null: false
     t.text "content"
@@ -27,8 +27,10 @@ ActiveRecord::Schema[7.2].define(version: 2024_08_22_202734) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "username"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
   add_foreign_key "posts", "users"
