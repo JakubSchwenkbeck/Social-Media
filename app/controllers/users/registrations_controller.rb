4adbef61f0abd352da257e1b3958d89b15bd540c
Jakub <jakub@schwenkbeck.de>
2024-08-23 12:28:33 +0200

    implemented profile pictures for each account which are displayed next to the posts

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -5,10 +5,10 @@ class Users::RegistrationsController < Devise::RegistrationsController
   protected
 
   def configure_sign_up_params
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :profile_picture])
   end
 
   def configure_account_update_params
-    devise_parameter_sanitizer.permit(:account_update, keys: [:username])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:username, :profile_picture])
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,24 @@
 class User < ApplicationRecord
-  # Devise modules and other validations
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :posts, dependent: :destroy
+  has_one_attached :profile_picture
 
   validates :username, presence: true, uniqueness: { case_sensitive: false }
+
+  # You might want to add validations for the profile picture, e.g.:
+  validate :acceptable_image
+
+  def acceptable_image
+    return unless profile_picture.attached?
+
+    unless profile_picture.byte_size <= 1.megabyte
+      errors.add(:profile_picture, "is too big")
+    end
+
+    acceptable_types = ["image/jpeg", "image/png"]
+    unless acceptable_types.include?(profile_picture.content_type)
+      errors.add(:profile_picture, "must be a JPEG or PNG")
+    end
+  end
 end
