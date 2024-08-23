9e74b2bfac91bb0dadc6af0d09890a4f1b4cec55
Jakub <jakub@schwenkbeck.de>
2024-08-23 22:26:53 +0200

    Changed to enable Biographies for each user and new styling.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,7 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:account_update, keys: [:profile_picture, :biography])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :profile_picture, :biography])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:username, :profile_picture, :biography])
   end
 end
