1b9d7082e678aa0f6fd7fb96bce66f7fed181e6e
Jakub <jakub@schwenkbeck.de>
2024-08-23 13:19:43 +0200

    âdded new html and (s)css stylings

--- Gemfile
+++ Gemfile
@@ -21,6 +21,7 @@ gem "jbuilder"
 gem "devise"
 # Use Kredis to get higher-level data types in Redis [https://github.com/rails/kredis]
 # gem "kredis"
+gem 'sassc-rails'
 
 # Use Active Model has_secure_password [https://guides.rubyonrails.org/active_model_basics.html#securepassword]
 # gem "bcrypt", "~> 3.1.7"
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_08_23_101702) do
+ActiveRecord::Schema[7.2].define(version: 2024_08_23_103917) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -44,6 +44,7 @@ ActiveRecord::Schema[7.2].define(version: 2024_08_23_101702) do
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "title"
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
