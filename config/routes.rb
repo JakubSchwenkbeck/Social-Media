cbb8ec3f0445b58814073fc49a1f6cdfc0f3d839
Jakub <jakub@schwenkbeck.de>
2024-08-23 13:12:09 +0200

    signout button added

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,16 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: {
-  registrations: 'users/registrations'
-}
-  resources :posts, only: [:index, :new, :create]
+  # Devise routes for user authentication
+  devise_for :users, controllers: { registrations: 'users/registrations' }
+
+  # Define a route for user profiles
+  resources :users, only: [:show]
+
+  # Alternatively, a custom route
+  get 'profile/:id', to: 'users#show', as: 'profile'
+
+  # Define RESTful routes for posts, including the show action
+  resources :posts, only: [:index, :new, :create, :show]
+
+  # Define the root path
   root 'posts#index'
 end
